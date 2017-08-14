c71c912d59d18bb97bc15025b65db4726d4779e4
Sye Boddeus <sboddeus@Gwaihir.local>
2017-08-14 22:16:39 +0100

    Added quick comment
    Note in code to estabish a ‘full’ API stubb

--- GitHubBrowserUITests/GitHubBrowserUITests.swift
+++ GitHubBrowserUITests/GitHubBrowserUITests.swift
@@ -15,9 +15,8 @@ class GitHubBrowserUITests: XCTestCase {
 
     override func setUp() {
         super.setUp()
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
+        // At some point here I would insert a mock API fixture into our app to speed up testin
+
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
